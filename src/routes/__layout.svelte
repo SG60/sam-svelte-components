c36a22d5d09744d1edbbd80766900432bb47ff5d
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-04-29 00:37:28 +0100

    updates

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,27 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/stores';
+	import { base } from '$app/paths';
 </script>
+
+<header>
+	<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li class:active={$page.url.pathname === base + '/'}>
+				<a sveltekit:prefetch href="{base}/">Home</a>
+			</li>
+			<li class:active={$page.url.pathname === base + '/about'}>
+				<a sveltekit:prefetch href="{base}/about">About</a>
+			</li>
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
+</header>
 
 <main>
 	<slot />
