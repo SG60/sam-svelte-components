3b16ebde51aae7fea6521b4bae11e393d0ff51a4
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-05-28 12:52:55 +0000

    YouTube component

---
+++ src/lib/YouTube.svelte
@@ -0,0 +1,16 @@
+<script type="ts">
+	export let id: string;
+	export let title = 'YouTube Video';
+	const ytHost = 'https://www.youtube.com';
+	export let autoplay = false;
+</script>
+
+<div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
+	<iframe
+		loading="lazy"
+		src="{ytHost}/embed/{id}?autoplay={autoplay ? 1 : 0}"
+		style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; border:0;"
+		allowfullscreen
+		{title}
+	/>
+</div>
---
+++ src/stories/YouTube.stories.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import SoundCloud from '$lib/SoundCloud.svelte';
+	import YouTube from '$lib/YouTube.svelte';
+</script>
+
+<Meta
+	title="Embeds/YouTube"
+	component={SoundCloud}
+	argTypes={{
+		title: { control: 'text' },
+		id: { control: 'text' },
+		autoplay: { control: 'boolean' }
+	}}
+/>
+
+<Template let:args><YouTube {...args} /></Template>
+
+<Story name="default" args={{ id: 'dByckr7h1Zk' }} />
+<Story name="autoplay" args={{ id: 'dByckr7h1Zk', autoplay: true }} />
