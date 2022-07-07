d5a4323763bde68450cf2e711aa87ce888494a83
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-07-07 01:18:05 +0000

    update sveltekit to 1.0.0-next.360

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-import path from 'path';
-
 const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -21,13 +19,6 @@ const config = {
 		appDir: 'internal',
 		prerender: {
 			default: true
-		},
-		vite: {
-			resolve: {
-				alias: {
-					$utils: path.resolve('./src/lib/utils')
-				}
-			}
 		}
 	}
 };
