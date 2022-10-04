d3c9b24aed8d864fbc86a17ab233e69b8759ef77
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-10-04 02:25:30 +0100

    add YouTubeLite.svelte, closes #195

---
+++ src/lib/YouTubeLite.svelte
@@ -0,0 +1,13 @@
+<script type="ts">
+	export let id: string;
+	export let title = 'YouTube Video';
+
+	onMount(async () => {
+		await import('@justinribeiro/lite-youtube');
+	});
+	import { onMount } from 'svelte';
+</script>
+
+<div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
+	<lite-youtube videoid={id} videotitle={title} />
+</div>
---
+++ src/stories/YouTubeLite.stories.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import YouTubeLite from '$lib/YouTubeLite.svelte';
+</script>
+
+<Meta
+	title="Embeds/YouTube Lite"
+	component={YouTubeLite}
+	argTypes={{
+		title: { control: 'text' },
+		id: { control: 'text' }
+	}}
+/>
+
+<Template let:args><YouTubeLite {...args} /></Template>
+
+<Story
+	name="standard"
+	args={{ id: 'dByckr7h1Zk', title: 'Video Title' }}
+	parameters={{ chromatic: { disableSnapshot: true } }}
+/>
