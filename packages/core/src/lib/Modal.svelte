63019556bf8cbc5914fd95e446004bcee9c4a026
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-10-31 22:46:56 +0000

    Add Modal component

---
+++ packages/core/src/lib/Modal.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let dialog: HTMLDialogElement;
+
+	export let open: boolean;
+	$: {
+		if (dialog) {
+			if (open) dialog.showModal();
+			else dialog.close();
+		}
+	}
+
+	let extraClasses = '';
+	export { extraClasses as class };
+</script>
+
+<dialog on:close on:close={() => (open = false)} on:submit bind:this={dialog} class={extraClasses}>
+	<slot />
+	<form method="dialog">
+		<slot name="buttons" />
+	</form>
+</dialog>
---
+++ storybook/src/stories/core/Modal.stories.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import Modal from '@samsveltecomponents/core/Modal.svelte';
+
+	let open: boolean = false;
+</script>
+
+<Meta
+	title="Modal"
+	component={Modal}
+	argTypes={{
+		class: { control: 'text' }
+	}}
+/>
+
+<Template let:args>
+	<Modal {...args} bind:open>
+		<h1>A MODAL</h1>
+		<p>Modal here!</p>
+		<button slot="buttons">All done!</button></Modal
+	>
+	<button on:click={() => (open = true)}>Open Modal</button>
+</Template>
+
+<Story name="standard" args={{ class: 'modal-styles' }} />
+
+<style>
+	:global(.modal-styles) {
+		border-radius: 1rem;
+		box-shadow: #000000 4px 4px 20px -5px;
+		border-color: #b9bdc3;
+		border-width: 2px;
+	}
+</style>
