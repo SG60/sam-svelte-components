5016990c2314a4ff10d80396bb1abb7a8d8d477f
Sam Greening <2552620+SG60@users.noreply.github.com>
2023-04-24 13:06:59 +0100

    fix storybook
    add storybook dev command to turbo.json
    
    run storybook upgrade command

---
+++ docs/.storybook/main.js
@@ -0,0 +1,20 @@
+/** @type { import('@storybook/sveltekit').StorybookConfig } */
+const config = {
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-a11y',
+		'@storybook/addon-storysource',
+		'@storybook/addon-svelte-csf'
+	],
+	framework: {
+		name: '@storybook/sveltekit',
+		options: {}
+	},
+	docs: {
+		autodocs: 'tag'
+	}
+};
+export default config;
---
+++ docs/.storybook/preview.js
@@ -0,0 +1,17 @@
+// global css (includes tailwind imports)
+import '../src/app.css';
+
+/** @type { import('@storybook/svelte').Preview } */
+const preview = {
+	parameters: {
+		actions: { argTypesRegex: '^on[A-Z].*' },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/
+			}
+		}
+	}
+};
+
+export default preview;
