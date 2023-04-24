f5e7610f2b6270a030f059fad305e06e9f07956c
Sam Greening <2552620+SG60@users.noreply.github.com>
2023-04-24 13:02:50 +0100

    Sanity YouTube schema and component

---
+++ packages/sanity-tools/src/lib/YouTube/YouTube.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { CustomBlockComponentProps } from '@portabletext/svelte';
+	import YouTubeLite from '@samsveltecomponents/youtube-lite/YouTubeLite.svelte';
+
+	export let portableText: CustomBlockComponentProps<{
+		id: string;
+		title?: string;
+	}>;
+
+	const { id, title } = portableText.value;
+</script>
+
+<YouTubeLite {id} {title} />
