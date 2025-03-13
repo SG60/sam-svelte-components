b1f2549f6503c905c73af5c04c0dff8eb069c648
Sam Greening <2552620+SG60@users.noreply.github.com>
2025-03-13 15:00:05 +0000

    chore(youtube-lite): update to svelte v5

--- packages/youtube-lite/src/lib/YouTubeLite.svelte
+++ packages/youtube-lite/src/lib/YouTubeLite.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	export let id: string;
 	export let title = 'YouTube Video';
 
-	onMount(async () => {
-		await import('@justinribeiro/lite-youtube');
-	});
-	import { onMount } from 'svelte';
+	if (browser) {
+		// This custom element import only works in the browser
+		import('@justinribeiro/lite-youtube');
+	}
 </script>
 
 <div style="position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden;">
-	<lite-youtube videoid={id} videotitle={title} />
+	<lite-youtube videoid={id} videotitle={title}></lite-youtube>
 </div>
--- packages/youtube-lite/src/routes/+page.svelte
+++ packages/youtube-lite/src/routes/+page.svelte
@@ -1,3 +1,13 @@
+<script>
+	import { YouTubeLite } from '$lib';
+</script>
+
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h2>Test videos</h2>
+
+<div>
+	<YouTubeLite title="greening. bubbles" id="QUqohXJUJ2E" />
+</div>
